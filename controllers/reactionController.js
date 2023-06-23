21508f8c3459fea3898cef8c3953dfd29486ccaa
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-06-23 02:33:23 -0400

    Corrected the delete a reaction from a thought function, also added a get all reactions route

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,54 +1,63 @@
 const { Reaction } = require('../models');
 const { Thought } = require('../models');
+
 const reactionController = {
-  // POST add a reaction to a thought
-  addReaction({ params, body }, res) {
-    Reaction.create(body)
-      .then(reaction => {
-        return Thought.findByIdAndUpdate(
+// POST add a reaction to a thought
+addReaction({ params, body }, res) {
+  Reaction.create(body)
+    .then(reaction => {
+      return Promise.all([
+        Thought.findByIdAndUpdate(
           params.thoughtId,
           { $push: { reactions: reaction._id } },
           { new: true, runValidators: true }
-        );
-      })
-      .then(thought => {
-        if (!thought) {
-          res.status(404).json({ message: 'No thought found with this id' });
-          return;
-        }
-        res.json(thought);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
+        ),
+        User.findOneAndUpdate(
+          { username: body.username },
+          { $push: { reactions: reaction._id } },
+          { new: true, runValidators: true }
+        )
+      ]);
+    })
+    .then(([thought, user]) => {
+      if (!thought) {
+        res.status(404).json({ message: 'No thought found with this id' });
+        return;
+      }
+      if (!user) {
+        res.status(404).json({ message: 'No user found with this username' });
+        return;
+      }
+      res.json(thought);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+},
+
 
   // DELETE remove a reaction from a thought
   removeReaction({ params }, res) {
     Reaction.findByIdAndDelete(params.reactionId)
       .then(deletedReaction => {
         if (!deletedReaction) {
-          res.status(404).json({ message: 'No reaction found with this id' });
-          return;
+          return res.status(404).json({ message: 'No reaction found with this id' });
         }
         return Thought.findByIdAndUpdate(
           params.thoughtId,
           { $pull: { reactions: params.reactionId } },
-          { new: true, runValidators: true }
+          { new: true }
         );
       })
-      .then(thought => {
-        if (!thought) {
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
           res.status(404).json({ message: 'No thought found with this id' });
           return;
         }
-        res.json(thought);
+        res.json(dbThoughtData);
       })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
+      .catch(err => res.json(err));
   },
 
  // PUT update a reaction
@@ -70,7 +79,34 @@ const reactionController = {
       console.log(err);
       res.status(400).json(err);
     });
+},
+
+// GET all reactions
+getReactions(req, res) {
+  Thought.find()
+    .populate('reactions')
+    .select('-__v')
+    .then(thoughts => {
+      const reactions = thoughts.reduce((acc, thought) => {
+        acc.push(...thought.reactions.map(reaction => {
+          return {
+            thoughtId: thought._id,
+            reactionId: reaction._id,
+            reactionBody: reaction.reactionBody,
+            username: reaction.username,
+            createdAt: reaction.createdAt
+          };
+        }));
+        return acc;
+      }, []);
+      res.json(reactions);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 }
+
 };
 
 module.exports = reactionController;
--- routes/api/reactionsRoute.js
+++ routes/api/reactionsRoute.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const {
   addReaction,
   removeReaction,
-  updateReaction
+  updateReaction,
+  getReactions
 } = require('../../controllers/reactionController');
 
 // POST add a reaction to a thought
@@ -15,5 +16,8 @@ router.put('/:thoughtId/reactions/:reactionId', updateReaction);
 // DELETE remove a reaction from a thought
 router.delete('/:thoughtId/reactions/:reactionId', removeReaction);
 
+
+// GET get all reactions
+router.get('/', getReactions);
 
 module.exports = router;
